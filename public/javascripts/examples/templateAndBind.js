68ce46b6d2b62e745a0cf76b5599c83376acd010
Jiachi Liu <liu.jiach@husky.neu.edu>
2014-12-30 23:31:10 -0500

    Added examples on text display and form input

--- public/javascripts/examples/templateAndBind.js
+++ public/javascripts/examples/templateAndBind.js
@@ -16,4 +16,49 @@ app.controller("TextDisplayController", ['$scope', function ($scope) {
     $scope.computePrice = function () {
         product.cost = product.quantities * 2.0;
     };
+}]).controller("RepeatElementController", ['$scope', function ($scope) {
+    var students = [
+        {
+            id: 1,
+            firstName: 'John',
+            lastName: 'Liu'
+        },
+        {
+            id: 2,
+            firstName: 'Tedd',
+            lastName: 'Scott'
+        },
+        {
+            id: 3,
+            firstName: 'Vic',
+            lastName: 'Meritt'
+        }
+    ];
+
+    var toAdd = {
+        id: 4,
+        firstName: 'Student',
+        lastName: 'New'
+    };
+
+    $scope.students = students;
+
+    $scope.addStudent = function () {
+        students.push(toAdd);
+    }
+}]).controller("HideAndShowController", ["$scope", function ($scope) {
+    $scope.message = {
+        show: true,
+        content: "Controlled by ng-show",
+        style: 'error'
+    }
+
+    $scope.toggle = function(){
+        $scope.message.show = !$scope.message.show;
+        if($scope.message.style == 'error'){
+            $scope.message.style = 'notError';
+        }else{
+            $scope.message.style = 'error';
+        }
+    }
 }]);
